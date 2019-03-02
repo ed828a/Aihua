74039c5439dc1b0e1cb5eb1f8455dc7fcb98fde4
Edward <ed828a@gmail.com>
2019-03-02 12:07:45 +1030

    add ReCapcha

---
+++ app/src/main/java/com/dew/aihua/helper/PageDownloader.kt
@@ -0,0 +1,150 @@
+package com.dew.aihua.helper
+
+import android.text.TextUtils
+
+import org.schabi.newpipe.extractor.exceptions.ReCaptchaException
+import org.schabi.newpipe.extractor.utils.Localization
+
+import java.io.IOException
+import java.io.InputStream
+import java.util.HashMap
+import java.util.concurrent.TimeUnit
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.ResponseBody
+
+/**
+ *  Created by Edward on 3/2/2019.
+ */
+class PageDownloader private constructor(builder: OkHttpClient.Builder) : org.schabi.newpipe.extractor.Downloader {
+    var cookies: String? = null
+    private val client: OkHttpClient = builder
+        .readTimeout(30, TimeUnit.SECONDS)
+        //.cache(new Cache(new File(context.getExternalCacheDir(), "okhttp"), 16 * 1024 * 1024))
+        .build()
+
+    /**
+     * Get the size of the content that the url is pointing by firing a HEAD request.
+     *
+     * @param url an url pointing to the content
+     * @return the size of the content, in bytes
+     */
+    @Throws(IOException::class)
+    fun getContentLength(url: String): Long {
+        var response: Response? = null
+        try {
+            val request = Request.Builder()
+                .head().url(url)
+                .addHeader("User-Agent", USER_AGENT)
+                .build()
+            response = client.newCall(request).execute()
+
+            val contentLength = response!!.header("Content-Length")
+            return if (contentLength == null) -1 else java.lang.Long.parseLong(contentLength)
+        } catch (e: NumberFormatException) {
+            throw IOException("Invalid content length", e)
+        } finally {
+            response?.close()
+        }
+    }
+
+    /**
+     * Download the text file at the supplied URL as in download(String),
+     * but set the HTTP header field "Accept-Language" to the supplied string.
+     *
+     * @param siteUrl  the URL of the text file to return the contents of
+     * @param localization the language and country (usually a 2-character code) to set
+     * @return the contents of the specified text file
+     */
+    @Throws(IOException::class, ReCaptchaException::class)
+    override fun download(siteUrl: String, localization: Localization): String {
+        val requestProperties = HashMap<String, String>()
+        requestProperties["Accept-Language"] = localization.language
+        return download(siteUrl, requestProperties)
+    }
+
+    /**
+     * Download the text file at the supplied URL as in download(String),
+     * but set the HTTP headers included in the customProperties map.
+     *
+     * @param siteUrl          the URL of the text file to return the contents of
+     * @param customProperties set request header properties
+     * @return the contents of the specified text file
+     * @throws IOException
+     */
+    @Throws(IOException::class, ReCaptchaException::class)
+    override fun download(siteUrl: String, customProperties: Map<String, String>): String {
+        return getBody(siteUrl, customProperties)!!.string()
+    }
+
+    @Throws(IOException::class)
+    fun stream(siteUrl: String): InputStream {
+        try {
+            return getBody(siteUrl, emptyMap())!!.byteStream()
+        } catch (e: ReCaptchaException) {
+            throw IOException(e.message, e.cause)
+        }
+
+    }
+
+    @Throws(IOException::class, ReCaptchaException::class)
+    private fun getBody(siteUrl: String, customProperties: Map<String, String>): ResponseBody? {
+        val requestBuilder = Request.Builder()
+            .method("GET", null).url(siteUrl)
+            .addHeader("User-Agent", USER_AGENT)
+
+        for ((key, value) in customProperties) {
+            requestBuilder.addHeader(key, value)
+        }
+
+        if (!TextUtils.isEmpty(cookies)) {
+            requestBuilder.addHeader("Cookie", cookies!!)
+        }
+
+        val request = requestBuilder.build()
+        val response = client.newCall(request).execute()
+        val body = response.body()
+
+        if (response.code() == 429) {
+            throw ReCaptchaException("reCaptcha Challenge requested")
+        }
+
+        if (body == null) {
+            response.close()
+            return null
+        }
+
+        return body
+    }
+
+    /**
+     * Download (via HTTP) the text file located at the supplied URL, and return its contents.
+     * Primarily intended for downloading web pages.
+     *
+     * @param siteUrl the URL of the text file to download
+     * @return the contents of the specified text file
+     */
+    @Throws(IOException::class, ReCaptchaException::class)
+    override fun download(siteUrl: String): String {
+        return download(siteUrl, emptyMap())
+    }
+
+    companion object {
+        const val USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0"
+
+        var instance: PageDownloader? = null
+            private set
+
+        /**
+         * It's recommended to call exactly once in the entire lifetime of the application.
+         *
+         * @param builder if null, default builder will be used
+         */
+        fun init(builder: OkHttpClient.Builder?): PageDownloader {
+            instance = PageDownloader(builder ?: OkHttpClient.Builder())
+            return instance!!
+        }
+    }
+}
---
+++ app/src/main/java/com/dew/aihua/ui/activity/ReCaptchaActivity.kt
@@ -0,0 +1,135 @@
+package com.dew.aihua.ui.activity
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.webkit.CookieManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.core.app.NavUtils
+import com.dew.aihua.R
+import com.dew.aihua.helper.PageDownloader
+
+class ReCaptchaActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Suppress("DEPRECATION")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recaptcha)
+
+        // Set return to Cancel by default
+        setResult(Activity.RESULT_CANCELED)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setTitle(R.string.reCaptcha_title)
+            actionBar.setDisplayShowTitleEnabled(true)
+        }
+
+        val myWebView = findViewById<WebView>(R.id.reCaptchaWebView)
+
+        // Enable Javascript
+        val webSettings = myWebView.settings
+        webSettings.javaScriptEnabled = true
+
+        val webClient = ReCaptchaWebViewClient(this)
+        myWebView.webViewClient = webClient
+
+        // Cleaning cache, history and cookies from webView
+        myWebView.clearCache(true)
+        myWebView.clearHistory()
+        val cookieManager = CookieManager.getInstance()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            cookieManager.removeAllCookies { }
+        } else {
+            cookieManager.removeAllCookie()
+        }
+
+        myWebView.loadUrl(YT_URL)
+    }
+
+    private inner class ReCaptchaWebViewClient internal constructor(private val context: Activity) : WebViewClient() {
+        private var mCookies: String? = null
+
+
+        override fun onPageStarted(view: WebView, url: String, favicon: Bitmap) {
+            // Start Loader
+            super.onPageStarted(view, url, favicon)
+            Log.d(TAG, "onPageStarted(): url = $url")
+            Toast.makeText(context, "ReCaptcha is doing.", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onPageFinished(view: WebView, url: String) {
+            val cookies = CookieManager.getInstance().getCookie(url)
+
+            // Stop Loader
+
+            // find cookies : s_gl & goojf and Add cookies to Downloader
+            if (findAccessCookies(cookies)) {
+                // Give cookies to Downloader class
+                PageDownloader.instance!!.cookies = mCookies
+
+                // Closing activity and return to parent
+                setResult(Activity.RESULT_OK)
+                finish()
+            }
+        }
+
+        private fun findAccessCookies(cookies: String): Boolean {
+            var ret = false
+            var cSGL = ""
+            var cGoojf = ""
+
+            val parts = cookies.split("; ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            for (part in parts) {
+                if (part.trim { it <= ' ' }.startsWith("s_gl")) {
+                    cSGL = part.trim { it <= ' ' }
+                }
+                if (part.trim { it <= ' ' }.startsWith("goojf")) {
+                    cGoojf = part.trim { it <= ' ' }
+                }
+            }
+            if (cSGL.isNotEmpty() && cGoojf.isNotEmpty()) {
+                ret = true
+                //mCookies = c_s_gl + "; " + c_goojf;
+                // Youtube seems to also need the other cookies:
+                mCookies = cookies
+            }
+
+            return ret
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        return when (id) {
+            android.R.id.home -> {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                NavUtils.navigateUpTo(this, intent)
+                true
+            }
+            else -> false
+        }
+    }
+
+    companion object {
+        const val RECAPTCHA_REQUEST = 10
+
+        val TAG = ReCaptchaActivity::class.java.toString()
+        const val YT_URL = "https://www.youtube.com"
+    }
+}
