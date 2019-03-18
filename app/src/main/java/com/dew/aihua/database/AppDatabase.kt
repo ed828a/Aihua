4c7be178484de764cba02375bb958fbbd04602d0
Edward <ed828a@gmail.com>
2019-03-18 15:35:06 +1030

    add sudo code for database migration

--- app/src/main/java/com/dew/aihua/database/AppDatabase.kt
+++ app/src/main/java/com/dew/aihua/database/AppDatabase.kt
@@ -5,6 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
 import com.dew.aihua.database.downloadDB.DownloadDAO
 import com.dew.aihua.database.downloadDB.MissionEntity
 import com.dew.aihua.database.history.dao.SearchHistoryDAO
@@ -23,25 +24,29 @@ import com.dew.aihua.database.stream.model.StreamEntity
 import com.dew.aihua.database.stream.model.StreamStateEntity
 import com.dew.aihua.database.subscription.SubscriptionDAO
 import com.dew.aihua.database.subscription.SubscriptionEntity
+import androidx.sqlite.db.SupportSQLiteDatabase
+
 
 /**
  *  Created by Edward on 3/2/2019.
  */
 
 @TypeConverters(Converters::class)
-@Database(entities = [
-    SubscriptionEntity::class,
-    SearchHistoryEntry::class,
-    StreamEntity::class,
-    StreamHistoryEntity::class,
-    StreamStateEntity::class,
-    PlaylistEntity::class,
-    PlaylistStreamEntity::class,
-    PlaylistRemoteEntity::class,
-    MissionEntity::class
-],
+@Database(
+    entities = [
+        SubscriptionEntity::class,
+        SearchHistoryEntry::class,
+        StreamEntity::class,
+        StreamHistoryEntity::class,
+        StreamStateEntity::class,
+        PlaylistEntity::class,
+        PlaylistStreamEntity::class,
+        PlaylistRemoteEntity::class,
+        MissionEntity::class
+    ],
     version = 1,
-    exportSchema = false)
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun subscriptionDAO(): SubscriptionDAO
@@ -66,6 +71,14 @@ abstract class AppDatabase : RoomDatabase() {
 
         const val DATABASE_NAME = "aihua.db"
 
+        // in case Database changes later
+//        private val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+//            override fun migrate(database: SupportSQLiteDatabase) {
+//                // Since we didn't alter the table, there's nothing else to do here.
+//            }
+//        }
+
+
         // make this class a singleton
         @Volatile
         private var INSTANCE: AppDatabase? = null
@@ -76,7 +89,9 @@ abstract class AppDatabase : RoomDatabase() {
                 INSTANCE ?: Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    DATABASE_NAME)
+                    DATABASE_NAME
+                )
+//                    .addMigrations(MIGRATION_1_2)
                     .build()
                     .also { INSTANCE = it }
             }
